48dc5049ca0b45ec880693c5ab4b3d0190ac7501
Hai Nguyen <nhuhai.h.nguyen@gmail.com>
2016-08-06 18:06:45 -0700

    add flash-restful

--- route.py
+++ route.py
@@ -2,10 +2,20 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask_restful import Resource, Api
+import config
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
+api.add_resource(HelloWorld, '/')
+
+if __name__ == "__main__":
+  if config.app['debug']:
+    app.run(debug = True, threaded = True, host = config.app['host'], port = config.app['port'])
+  else:
+    app.run(debug = False, threaded = True)
